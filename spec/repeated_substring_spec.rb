ba04cc0504bef6307f80e8f446e714ae188c5fe0
Cyril Kyburz <cyril.kyburz@renuo.ch>
2016-01-21 09:31:43 +0100

    add sample data

--- spec/repeated_substring_spec.rb
+++ spec/repeated_substring_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe 'repeated_substring' do
     rs = RepeatedSubstring.new
     expect(rs.find_repeated_substring('banana')).to eq(3)
   end
+
+  xit 'find the most repeated substring for every line of a file' do
+    expect(RepeatedSubstring.new.find_repeated_substring_file('data/sample.txt')).to eq(['an', 'NONE', 'NONE', 'oo'])
+  end
 end
